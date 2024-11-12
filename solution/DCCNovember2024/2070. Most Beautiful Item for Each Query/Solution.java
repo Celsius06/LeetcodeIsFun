ff4517833ea163df77bfa917d68e12cce5e1cfa9
mthn.04 <minhthuan24062004@gmail.com>
2024-11-12 11:29:34 +0700

    DCCNov24_#2070 (soln added)

---
+++ solution/DCCNovember2024/2070. Most Beautiful Item for Each Query/Solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        Arrays.sort(items, (a, b) -> {
+            if (a[0] != b[0]) {
+                return Integer.compare(a[0], b[0]);
+            } else {
+                return Integer.compare(b[1], a[1]); 
+            }
+        });
+        
+        int n = items.length;
+        int[] maxBeauty = new int[n];
+        maxBeauty[0] = items[0][1]; 
+        for (int i = 1; i < n; i++) {
+            maxBeauty[i] = Math.max(maxBeauty[i - 1], items[i][1]);
+        }
+        
+        int m = queries.length;
+        int[] answer = new int[m];
+        
+        for (int i = 0; i < m; i++) {
+            int query = queries[i];
+            int left = 0, right = n - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (items[mid][0] <= query) {
+                    left = mid + 1; 
+                } else {
+                    right = mid - 1; 
+                }
+            }
+
+            if (right >= 0) {
+                answer[i] = maxBeauty[right];
+            } else {
+                answer[i] = 0; 
+            }
+        }
+        
+        return answer;
+    }
+}
