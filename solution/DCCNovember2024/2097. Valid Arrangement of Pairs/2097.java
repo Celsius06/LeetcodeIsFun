5d7346783429bd199f34a649638227ae6d55a89d
mthn.04 <minhthuan24062004@gmail.com>
2024-11-30 15:17:32 +0700

    update soln for #2097

--- solution/DCCNovember2024/2097. Valid Arrangement of Pairs/2097.java
+++ solution/DCCNovember2024/2097. Valid Arrangement of Pairs/2097.java
@@ -15,7 +15,7 @@ class Solution {
         Map<Integer, Stack<Integer>> graph = new HashMap<>();
         Map<Integer, Integer> outDegree = new HashMap<>();
         Map<Integer, Integer> inDegree = new HashMap<>();
-        
+
         for (int[] pair : pairs) {
             int start = pair[0], end = pair[1];
             graph.putIfAbsent(start, new Stack<>());
@@ -23,7 +23,7 @@ class Solution {
             outDegree.put(start, outDegree.getOrDefault(start, 0) + 1);
             inDegree.put(end, inDegree.getOrDefault(end, 0) + 1);
         }
-        
+
         // Step 2: Find the starting node
         int startNode = pairs[0][0]; // Default start
         for (int node : graph.keySet()) {
@@ -32,12 +32,12 @@ class Solution {
                 break;
             }
         }
-        
+
         // Step 3: Perform Hierholzer's algorithm
         List<int[]> result = new ArrayList<>();
         Deque<Integer> stack = new ArrayDeque<>();
         stack.push(startNode);
-        
+
         while (!stack.isEmpty()) {
             int node = stack.peek();
             if (graph.containsKey(node) && !graph.get(node).isEmpty()) {
@@ -45,11 +45,11 @@ class Solution {
             } else {
                 stack.pop();
                 if (!stack.isEmpty()) {
-                    result.add(new int[]{stack.peek(), node});
+                    result.add(new int[] { stack.peek(), node });
                 }
             }
         }
-        
+
         // Step 4: Return result
         Collections.reverse(result);
         return result.toArray(new int[0][0]);
