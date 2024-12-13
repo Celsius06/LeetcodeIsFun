30cc02e416edb2ade71dbd5d97ae94ab71812654
mthn.04 <minhthuan24062004@gmail.com>
2024-12-13 21:13:11 +0700

    2593 (updates)

--- solution/DCCDecember2024/2593. Find Score of an Array After Marking All Elements/2593.java
+++ solution/DCCDecember2024/2593. Find Score of an Array After Marking All Elements/2593.java
@@ -1,13 +1,38 @@
 // 2593. Find Score of an Array After Marking All Elements
 
-// Method
-// Runtime: 
+// Priority Queue (Min Heap)
+// Runtime: 184ms
 
 // Complexity:
-// + Time: 
-// + Space: 
+// + Time: O(n * log(n))
+// + Space: O(n)
+
+import java.util.PriorityQueue;
 
 class Solution {
-    
+    public long findScore(int[] nums) {
+        int n = nums.length;
+        boolean[] vis = new boolean[n];
+        PriorityQueue<int[]> q
+            = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        for (int i = 0; i < n; ++i) {
+            q.offer(new int[] {nums[i], i});
+        }
+        long ans = 0;
+        while (!q.isEmpty()) {
+            var p = q.poll();
+            ans += p[0];
+            vis[p[1]] = true;
+            for (int j : List.of(p[1] - 1, p[1] + 1)) {
+                if (j >= 0 && j < n) {
+                    vis[j] = true;
+                }
+            }
+            while (!q.isEmpty() && vis[q.peek()[1]]) {
+                q.poll();
+            }
+        }
+        return ans;
+    }
 }
 
