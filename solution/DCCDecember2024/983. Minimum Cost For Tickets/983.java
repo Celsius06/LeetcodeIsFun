4b69d88a93a6b7da26b0ae653ccd1b4904d06443
mthn.04 <minhthuan24062004@gmail.com>
2025-01-01 13:09:38 +0700

    983

--- solution/DCCDecember2024/983. Minimum Cost For Tickets/983.java
+++ solution/DCCDecember2024/983. Minimum Cost For Tickets/983.java
@@ -15,19 +15,21 @@ class Solution {
     }
 
     private int dfs(int i, int[] days, int[] costs, int[] memo) {
-        if (i >= days.length) return 0;
-        if (memo[i] != -1) return memo[i];
+        if (i >= days.length)
+            return 0;
+        if (memo[i] != -1)
+            return memo[i];
 
         int oneDay = costs[0] + dfs(i + 1, days, costs, memo);
         int j = i;
-        while (j < days.length && days[j] < days[i] + 7) j++;
+        while (j < days.length && days[j] < days[i] + 7)
+            j++;
         int sevenDays = costs[1] + dfs(j, days, costs, memo);
         j = i;
-        while (j < days.length && days[j] < days[i] + 30) j++;
+        while (j < days.length && days[j] < days[i] + 30)
+            j++;
         int thirtyDays = costs[2] + dfs(j, days, costs, memo);
 
         return memo[i] = Math.min(oneDay, Math.min(sevenDays, thirtyDays));
     }
 }
-
-
