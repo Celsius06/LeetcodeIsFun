16d6b83a24a3a228f92a9df2541e1f1eb0bc76fa
mthn.04 <minhthuan24062004@gmail.com>
2025-01-02 22:32:55 +0700

    2559 (update soln #1)

--- solution/2025/DCCJanuary/2559. Count Vowel Strings in Ranges/2559.java
+++ solution/2025/DCCJanuary/2559. Count Vowel Strings in Ranges/2559.java
@@ -1,14 +1,42 @@
 // 2559. Count Vowel Strings in Ranges
 
-// DSAs
-// Methods & Approaches
-// Runtime:
+// 1st Approach
+// Prefix Sum Array, Range Query Handling
+// Runtime: 6ms
 
 // Complexity:
-// + Time:
-// + Space:
+// + Time: O(n + m)
+// + Space: O(n + m)
 
 class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int n = words.length;
+        int[] valid = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (isVowel(words[i].charAt(0)) && isVowel(words[i].charAt(words[i].length() - 1))) {
+                valid[i] = 1;
+            } else {
+                valid[i] = 0;
+            }
+        }
 
+        // Prefix sum array
+        int[] prefix = new int[n];
+        prefix[0] = valid[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + valid[i];
+        }
+
+        // Answer queries
+        int[] result = new int[queries.length];
+        for (int q = 0; q < queries.length; q++) {
+            int li = queries[q][0], ri = queries[q][1];
+            result[q] = prefix[ri] - (li > 0 ? prefix[li - 1] : 0);
+        }
+        return result;
+    }
+
+    private boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
+    }
 }
-
