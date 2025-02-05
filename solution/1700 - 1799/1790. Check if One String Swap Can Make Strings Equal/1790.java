6d340d05bb0264cb6ba68334b92747d859d5a292
mthn.04 <minhthuan24062004@gmail.com>
2025-02-05 20:27:20 +0700

    #1790 (saved)

---
+++ solution/1700 - 1799/1790. Check if One String Swap Can Make Strings Equal/1790.java
@@ -0,0 +1,14 @@
+// 1790. Check if One String Swap Can Make Strings Equal
+// Difficulty: Easy
+
+// DSAs
+// Methods & Approaches
+// Runtime: _ms (05/02/2025 | 8pm - GMT+7)
+
+// Complexity:
+// + Time: O()
+// + Space: O()
+
+class Solution {
+    
+}
