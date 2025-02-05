65260be83d7a83ed9bf0d090d875d1bcbb433690
mthn.04 <minhthuan24062004@gmail.com>
2025-02-05 20:30:52 +0700

    #1790 (saved)

--- solution/1700 - 1799/1790. Check if One String Swap Can Make Strings Equal/1790.java
+++ solution/1700 - 1799/1790. Check if One String Swap Can Make Strings Equal/1790.java
@@ -6,9 +6,34 @@
 // Runtime: _ms (05/02/2025 | 8pm - GMT+7)
 
 // Complexity:
-// + Time: O()
-// + Space: O()
+// + Time: O(n)
+// + Space: O(1)
 
 class Solution {
-    
+    public boolean areAlmostEqual(String s1, String s2) {
+        int first = -1, second = -1;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (first == -1) {
+                    first = i;
+                } else if (second == -1) {
+                    second = i;
+                } else {
+                    // More than two differences
+                    return false;
+                }
+            }
+        }
+        // Check cases based on number of differences
+        if (first == -1) {
+            // No differences
+            return true;
+        } else if (second == -1) {
+            // Exactly one difference (can't swap)
+            return false;
+        } else {
+            // Check if swapping makes them equal
+            return s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first);
+        }
+    }
 }
