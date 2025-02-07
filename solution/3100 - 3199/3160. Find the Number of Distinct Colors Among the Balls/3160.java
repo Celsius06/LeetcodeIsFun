1f8c8f9db1c938667fad8079cadc6904b5c70181
mthn.04 <minhthuan24062004@gmail.com>
2025-02-07 21:55:05 +0700

    save

---
+++ solution/3100 - 3199/3160. Find the Number of Distinct Colors Among the Balls/3160.java
@@ -0,0 +1,17 @@
+// 3160. Find the Number of Distinct Colors Among the Balls
+// Difficulty: Medium
+
+// DSAs
+// Methods & Approaches
+// Runtime: _ms (07/02/2025 | 10pm - GMT+7)
+
+// Complexity:
+// + Time: O()
+// + Space: O()
+
+class Solution {
+    public int[] queryResults(int limit, int[][] queries) {
+        
+    }
+}
+
