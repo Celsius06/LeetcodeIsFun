1c8d2a5a997a69e38dfa87f82a48ca7ec2d6b8a1
mthn.04 <minhthuan24062004@gmail.com>
2025-02-15 21:50:53 +0700

    save

---
+++ solution/2600 - 2699/2698. Find the Punishment Number of an Integer/2698.java
@@ -0,0 +1,17 @@
+// 2698. Find the Punishment Number of an Integer
+// Difficulty: Medium
+// Link: https://leetcode.com/problems/find-the-punishment-number-of-an-integer/
+
+// DSAs
+// Methods & Approaches
+// Runtime: _ms (15/02/2025 | 10pm - GMT+7)
+
+// Complexity:
+// + Time: O()
+// + Space: O()
+
+class Solution {
+    public int punishmentNumber(int n) {
+
+    }
+}
