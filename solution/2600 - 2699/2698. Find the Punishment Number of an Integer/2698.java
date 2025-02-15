ccd21421ff244c8cf469e10d907c07e8399907d7
mthn.04 <minhthuan24062004@gmail.com>
2025-02-15 22:11:17 +0700

    #2698 (saved)

--- solution/2600 - 2699/2698. Find the Punishment Number of an Integer/2698.java
+++ solution/2600 - 2699/2698. Find the Punishment Number of an Integer/2698.java
@@ -4,14 +4,38 @@
 
 // DSAs
 // Methods & Approaches
-// Runtime: _ms (15/02/2025 | 10pm - GMT+7)
+// Runtime: 24ms (15/02/2025 | 10pm - GMT+7)
 
 // Complexity:
-// + Time: O()
-// + Space: O()
+// + Time: O(n * log(n)) | n = input number
+// + Space: O(1) | no extra space
 
 class Solution {
     public int punishmentNumber(int n) {
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            String s = String.valueOf((long) i * i);
+            if (canPartition(s, i, 0)) {
+                total += i * i;
+            }
+        }
+        return total;
+    }
 
+    private boolean canPartition(String s, int target, int index) {
+        if (index == s.length())
+            return target == 0;
+        if (target < 0)
+            return false;
+
+        long num = 0;
+        for (int i = index; i < s.length(); i++) {
+            num = num * 10 + (s.charAt(i) - '0');
+            if (num > target)
+                break;
+            if (canPartition(s, target - (int) num, i + 1))
+                return true;
+        }
+        return false;
     }
 }
